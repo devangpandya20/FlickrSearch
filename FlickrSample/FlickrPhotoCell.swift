0a6338634ec77f6be0c8cd8262f9025b6994d009
Zensar <zensar@Zensars-Mac-mini-3.local>
2017-08-01 18:03:44 +0530

    Firs Commit

---
+++ FlickrSample/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSample
+//
+//  Created by Zensar on 01/08/17.
+//  Copyright © 2017 Zensar. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
